45632c12e02dffa7ace88980d39eaa6884be63b3
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-11-26 12:11:12 +0100

    gitflow-feature-stash: StepWidget add

---
+++ src/components/UI/stepWidget/StepWrapper.tsx
@@ -0,0 +1,9 @@
+
+
+export const StepWrapper = () => {
+    return (
+        <div>
+            HGoal
+        </div>
+    )
+}
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StepWrapper } from '../components/UI/stepWidget/StepWrapper';
+
+export const TestPage = () => {
+    return (
+        <>
+            <StepWrapper/>
+        </>
+    )
+}
