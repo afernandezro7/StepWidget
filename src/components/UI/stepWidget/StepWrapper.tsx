efbe65a7c8b261a33405506c513640a6818fd1dd
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-11-26 16:38:04 +0100

    adding stepper

--- src/components/UI/stepWidget/StepWrapper.tsx
+++ src/components/UI/stepWidget/StepWrapper.tsx
@@ -6,18 +6,22 @@ export const StepWrapper = () => {
             <div className="steps-wrapper">
                 <div className="steps-progressbar">
                     <div className="stepper-wrapper">
-                        <div className="stepper-item completed">
-                            <div className="step-counter">1</div>
-                            <div className="step-name">First</div>
-                        </div>
                         <div className="stepper-item active">
+                            <div className="step-counter">&#10004;</div>
+                            {/* <div className="step-name">First</div> */}
+                            <div className="step-marker"></div>
+                        </div>
+                        <div className="stepper-item ">
                             <div className="step-counter">2</div>
-                            <div className="step-name">Second</div>
+                            {/* <div className="step-name">Second</div> */}
+                            <div className="step-marker"></div>
                         </div>
-                        <div className="stepper-item">
+                        <div className="stepper-item ">
                             <div className="step-counter">3</div>
-                            <div className="step-name">Third</div>
+                            {/* <div className="step-name">Third</div> */}
+                            <div className="step-marker"></div>
                         </div>
+                        
                     </div>
 
                 </div>
